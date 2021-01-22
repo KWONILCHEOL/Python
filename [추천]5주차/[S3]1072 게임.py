a3de8460de4db6401af9702852ee08491d38cf11
KWONILCHEOL <kwon6460@gmail.com>
2021-01-23 03:08:44 +0900

    update

--- [추천]5주차/[S3]1072 게임.py
+++ [추천]5주차/[S3]1072 게임.py
@@ -1,9 +1,11 @@
-# [S3] 게임
+# [S3]1072 게임
 # https://www.acmicpc.net/problem/1072
 # 시간 복잡도 : O(logn)
+# /(나누기)는 float형으로 나와서 조심해야 함
 
 def get_rate(x,y):
-  return int(y/x*100)
+  return 100 * y // x
+  #return int(y/x*100) - 50 29 -> 27나옴
 
 x, y = map(int, input().split())
 z = get_rate(x,y)
@@ -11,15 +13,15 @@ z = get_rate(x,y)
 if z >= 99:
   print(-1)
 else:
-  lo = 1
-  hi = int(1e9)
-  while lo <= hi:
+  lo = 0
+  hi = int(2e9)
+
+  while lo + 1 < hi:
     mid = (lo + hi) // 2
     nx, ny = x + mid, y + mid
     nz = get_rate(nx,ny)
-    print(z, nz, lo,mid,hi)
     if z == nz:
-      lo = mid + 1
+      lo = mid
     else:
-      hi = mid - 1
-  print(lo)
+      hi = mid
+  print(hi)
