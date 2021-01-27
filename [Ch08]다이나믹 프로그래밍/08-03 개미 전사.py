9579d8277b369eb53b59ce87591bff24c7a21e50
KWONILCHEOL <kwon6460@gmail.com>
2021-01-27 16:45:27 +0900

    Update 08-03 개미 전사.py

--- [Ch08]다이나믹 프로그래밍/08-03 개미 전사.py
+++ [Ch08]다이나믹 프로그래밍/08-03 개미 전사.py
@@ -8,10 +8,9 @@ arr = list(map(int, input().split()))
 
 d = [0] * (n + 1)
 d[0] = arr[0]
-d[1] = arr[1]
-d[2] = arr[0] + arr[2]
+d[1] = max(arr[0], arr[1])
 
-for i in range(3, n):
-  d[i] = max(d[i-2], d[i-3]) + arr[i]
+for i in range(2, n):
+  d[i] = max(d[i-1], d[i-2] + arr[i])
 
 print(d[n - 1])
