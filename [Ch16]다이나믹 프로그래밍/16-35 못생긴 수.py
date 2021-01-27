a271323e3f0ea2ff59c3cc68808a263bae65733c
KWONILCHEOL <kwon6460@gmail.com>
2021-01-28 00:59:59 +0900

    update

--- [Ch16]다이나믹 프로그래밍/16-35 못생긴 수.py
+++ [Ch16]다이나믹 프로그래밍/16-35 못생긴 수.py
@@ -1,5 +1,33 @@
 # [Ch16] 35 못생긴 수
-# 
+"""
+ ┌1  2  3  4  5  6  8  9  10
+2│   4  6  8  10 12    
+3│   6  9  12
+5│   10 15
+"""
 
 import sys
 input = sys.stdin.readline
+
+n = int(input())
+arr = [0] * n
+arr[0] = 1
+
+i2 = i3 = i5 = 0
+next2, next3, next5 = 2,3,5
+
+for i in range(1,n):
+  arr[i] = min(next2,next3,next5)
+
+  print(arr, next2, next3, next5)
+  if arr[i] == next2:
+    i2 += 1
+    next2 = arr[i2] * 2
+  if arr[i] == next3:
+    i3 += 1
+    next3 = arr[i3] * 3
+  if arr[i] == next5:
+    i5 += 1
+    next5 = arr[i5] * 5
+
+print(arr[n-1])
--- [Ch16]다이나믹 프로그래밍/16-36 편집 거리.py
+++ [Ch16]다이나믹 프로그래밍/16-36 편집 거리.py
@@ -3,3 +3,28 @@
 
 import sys
 input = sys.stdin.readline
+
+arr1 = input().rstrip()
+arr2 = input().rstrip()
+
+len1 = len(arr1) + 1
+len2 = len(arr2) + 1
+dp = [[0] * len2 for _ in range(len1)]
+for i in range(len1):
+  dp[i][0] = i
+  
+for j in range(len2):
+  dp[0][j] = j
+
+for a1 in arr1:
+  for a2 in arr2:
+    if a1 == a2:
+      dp[i][j] = dp[i-1][j-1]
+    else:
+      dp[i][j] = min(dp[i-1][j], dp[i-1][j-1], dp[i][j-1]) + 1
+
+print(dp)
+print(dp[len1-1][len2-1])
+
+# for i in range(len1 + 1):
+#   for j in range(len2 + 1):
