f14cd9855838afc209481e77405562711bdca34d
KWONILCHEOL <kwon6460@gmail.com>
2021-02-01 07:46:58 +0900

    Create [G4]17142 연구소 3.py

---
+++ STUDY/Dynamic Programming/[G4]17142 연구소 3.py
@@ -0,0 +1,34 @@
+# [G4]17142 연구소 3
+# https://www.acmicpc.net/problem/17142
+
+import sys
+from bisect import bisect_left
+input = sys.stdin.readline
+
+n = int(input())
+arr = []
+
+for i in range(n):
+    arr.append(list(map(int, input().split())))
+
+arr.sort()
+dp = []
+connect = [[0]]
+for i,x in enumerate(arr):
+    idx = bisect_left(dp, x[1])
+    
+    # if idx < len(dp) == True이면
+    # 이전 dp[idx]는 dp[idx-1]과 연관있음
+    if idx < len(dp):
+        dp[idx] = x[1]
+        connect[idx + 1] = connect[idx] + [x[0]]
+    else:
+        dp.append(x[1])
+        connect.append(connect[-1] + [x[0]])
+    arr[i] = x[0]
+
+ans = set(arr).difference(set(connect[-1]))
+
+print(len(ans))
+for x in ans:
+    print(x)
