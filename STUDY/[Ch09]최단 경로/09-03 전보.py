35cddabda89a22dd553e7a6eb1478e80db035aac
KWONILCHEOL <kwon6460@gmail.com>
2021-02-03 18:00:15 +0900

    7주차

--- STUDY/[Ch09]최단 경로/09-03 전보.py
+++ STUDY/[Ch09]최단 경로/09-03 전보.py
@@ -1,2 +1,12 @@
 import sys
 input = sys.stdin.readline
+
+INF = int(1e9)
+n,m,c = map(int,input().split())
+
+city = [[] for _ in range(n + 1)]
+distance = [INF] * (n + 1)
+
+for _ in range(m):
+    
+    
--- STUDY/[Ch09]최단 경로/다익스트라 O(nlogn).py
+++ STUDY/[Ch09]최단 경로/다익스트라 O(nlogn).py
@@ -12,18 +12,18 @@ for _ in range(m):
     a,b,c = map(int, input().split())
     graph[a].append((b,c))
 
-    def dijkstra(start):
-        q = []
-        heapq.heappush(q, (0, start))
-        distance[start] = 0
-        while q:
-            dist, now = heapq.heappop(q)
-            if distance[now] < dist:
-                continue
+def dijkstra(start):
+    q = []
+    heapq.heappush(q, (0, start))
+    distance[start] = 0
+    while q:
+        dist, now = heapq.heappop(q)
+        if distance[now] < dist:
+            continue
 
-            for i in graph[now]:
-                cost = dist + i[1]
+        for i in graph[now]:
+            cost = dist + i[1]
 
-                if cost < distance[i[0]]:
-                    distance[i[0]] = cost
-                    heapq.heappush(q, (cost, i[0]))
+            if cost < distance[i[0]]:
+                distance[i[0]] = cost
+                heapq.heappush(q, (cost, i[0]))
